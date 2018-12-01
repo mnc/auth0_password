1b357a486199ab7b53178ac368dd07f10e85d745
Nobuya SASAKI <manchose9@gmail.com>
2018-12-02 02:56:50 +0900

    enable to specify password strength

--- lib/auth0_password.rb
+++ lib/auth0_password.rb
@@ -7,8 +7,10 @@ class Auth0Password
   UPPERCASES    = ('a'..'z').to_a
   NUMBERS       = (0..9).to_a
   SPECIAL_CHARS = %w|! @ # $ % ^ & *|
+  STRENGTH      = %i|excellent good fair low|
 
-  def initialize(min_length=8)
+  def initialize(strength: , min_length: 8)
+    @strength   = strength
     @min_length = min_length
   end
 
@@ -16,33 +18,66 @@ class Auth0Password
   def generate(length)
     password_length = @min_length > length ? @min_length : length
     logger.warn("length parameter #{length} is less than min_length #{@min_length}") if @min_length > length
-    required_elements = required_lowercases + required_uppercases + required_numbers + required_special_chars
-    available_chars.tap do |a|
-      random_elements = password_length.times.map { a.sample }
-      break (required_elements + random_elements).join
+    case @strength
+    when :excellent
+      excellent(password_length)
+    when :good
+      good(password_length)
+    when :fair
+      fair(password_length)
+    when :low
+      low(password_length)
+    else
+      excellent(password_length)
     end
   end
 
   private
 
-  def available_chars
-    LOWERCASES + UPPERCASES + NUMBERS + SPECIAL_CHARS
+  def excellent(length)
+    required_elements = random_lowercase + random_uppercase + random_number + random_special_char
+    (LOWERCASES + UPPERCASES + NUMBERS + SPECIAL_CHARS).tap do |a|
+      random_elements = length.times.map { a.sample }
+      break (required_elements + random_elements).shuffle.join
+    end
   end
 
-  def required_lowercases(size=1)
-    LOWERCASES.sample(size)
+  def good(length)
+    required_elements = random_lowercase + random_uppercase + random_number + random_special_char
+    (LOWERCASES + UPPERCASES + NUMBERS + SPECIAL_CHARS).tap do |a|
+      random_elements = length.times.map { a.sample }
+      break (required_elements + random_elements).shuffle.join
+    end
   end
 
-  def required_uppercases(size=1)
-    UPPERCASES.sample(size)
+  def fair(length)
+    required_elements = random_lowercase + random_uppercase + random_number
+    (LOWERCASES + UPPERCASES + NUMBERS).tap do |a|
+      random_elements = length.times.map { a.sample }
+      break (required_elements + random_elements).shuffle.join
+    end
   end
 
-  def required_numbers(size=1)
-    NUMBERS.sample(size)
+  def low(length)
+    (LOWERCASES + UPPERCASES + NUMBERS + SPECIAL_CHARS).tap do |a|
+      break length.times.map { a.sample }.join
+    end
   end
 
-  def required_special_chars(size=1)
-    SPECIAL_CHARS.sample(size)
+  def random_lowercase
+    LOWERCASES.sample
+  end
+
+  def random_uppercase
+    UPPERCASES.sample
+  end
+
+  def random_number
+    NUMBERS.sample
+  end
+
+  def random_special_char
+    SPECIAL_CHARS.sample
   end
 
   def logger
