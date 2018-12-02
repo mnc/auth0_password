a3f4e7bb98ab4d9a5c0686fc311cb0ffb39fd17e
Nobuya SASAKI <manchose9@gmail.com>
2018-12-02 22:59:38 +0900

    split generate method into each password strength methods to make testable code.

--- lib/auth0_password.rb
+++ lib/auth0_password.rb
@@ -8,60 +8,56 @@ class Auth0Password
   NUMBERS       = ('0'..'9').to_a
   SPECIAL_CHARS = %w|! @ # $ % ^ & *|
   ALL_CHARS     = LOWERCASES + UPPERCASES + NUMBERS + SPECIAL_CHARS
-  STRENGTH = %i|excellent good fair low|
   DEFAULT_LENGTH = 8
 
-  def initialize(strength: , min_length: DEFAULT_LENGTH)
-    @strength   = strength
+  def initialize(min_length: DEFAULT_LENGTH)
     @min_length = min_length
   end
 
-  def generate(length=DEFAULT_LENGTH)
-    password_length = @min_length > length ? @min_length : length
-    logger.warn("length parameter #{length} is less than min_length #{@min_length}") if @min_length > length
-    if STRENGTH.include?(@strength)
-      send(@strength, password_length)
-    else
-      logger.warn("strength #{@strength} is invalid. use excellent strength instead")
-      excellent(password_length)
+  def excellent(length=DEFAULT_LENGTH)
+    rounded_up_length = roundup_length(length)
+    good_password = good(rounded_up_length)
+    replace_continuous_chars!(good_password)
+  end
+
+  def good(length=DEFAULT_LENGTH)
+    rounded_up_length = roundup_length(length)
+    required_chars = [random_lowercase, random_uppercase, random_number, random_special_char]
+    ALL_CHARS.tap do |a|
+      random_chars = rounded_up_length.times.map { a.sample }
+      break (required_chars + random_chars).shuffle.join
+    end
+  end
+
+  def fair(length=DEFAULT_LENGTH)
+    rounded_up_length = roundup_length(length)
+    required_chars = [random_lowercase, random_uppercase, random_number]
+    (LOWERCASES + UPPERCASES + NUMBERS).tap do |a|
+      random_chars = rounded_up_length.times.map { a.sample }
+      break (required_chars + random_chars).shuffle.join
+    end
+  end
+
+  def low(length=DEFAULT_LENGTH)
+    rounded_up_length = roundup_length(length)
+    ALL_CHARS.tap do |a|
+      break rounded_up_length.times.map { a.sample }.join
     end
   end
 
   private
 
-  def excellent(length)
-    good_password = good(length)
-    replace_continuous_chars!(good_password)
+  def roundup_length(length)
+    logger.warn("length parameter #{length} is less than min_length #{@min_length}") if @min_length > length
+    @min_length > length ? @min_length : length
   end
 
   def replace_continuous_chars!(password)
-    password.tap do |pass|
-      while m = /(.)\1+/.match(pass) do
+    password.tap do |pw|
+      while m = /(.)\1{2,}/.match(pw) do
         excepted_chars = ALL_CHARS - [m[1]]
-        pass[m.begin(0) + 1] = excepted_chars.sample
+        pw[m.begin(0) + 1] = excepted_chars.sample
       end
-    end
-  end
-
-  def good(length)
-    required_chars = [random_lowercase, random_uppercase, random_number, random_special_char]
-    ALL_CHARS.tap do |a|
-      random_chars = length.times.map { a.sample }
-      break (required_chars + random_chars).shuffle.join
-    end
-  end
-
-  def fair(length)
-    required_chars = [random_lowercase, random_uppercase, random_number]
-    (LOWERCASES + UPPERCASES + NUMBERS).tap do |a|
-      random_chars = length.times.map { a.sample }
-      break (required_chars + random_chars).shuffle.join
-    end
-  end
-
-  def low(length)
-    ALL_CHARS.tap do |a|
-      break length.times.map { a.sample }.join
     end
   end
 
